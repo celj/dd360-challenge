1978f2c46bf9c6de9b8651d73a99891c8cde43f1
Carlos Lezama <carloselezamaj@gmail.com>
2023-06-18 13:02:10 -0600

    add: settings

---
+++ dags/settings.py
@@ -0,0 +1,4 @@
+from pendulum import timezone
+
+TIMEZONE = "UTC"
+LOCAL_TZ = timezone(TIMEZONE)
--- dags/weather/weather.py
+++ dags/weather/weather.py
@@ -13,21 +13,20 @@ import os
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from dags.settings import LOCAL_TZ
 from dags.weather.utils import get_weather_data
 from datetime import datetime
-from pendulum import timezone
 
 # -------------------- Globals -------------------- #
 DAG_ID = "weather"
 QUERIES_BASE_PATH = os.path.join(os.path.dirname(__file__), "queries")
-TIMEZONE = "UTC"
 
 with DAG(
     dag_id=DAG_ID,
     catchup=False,
     max_active_runs=1,
     schedule_interval="0 * * * *",
-    start_date=datetime(2023, 6, 15, tzinfo=timezone(TIMEZONE)),
+    start_date=datetime(2023, 6, 15, tzinfo=LOCAL_TZ),
     template_searchpath=QUERIES_BASE_PATH,
     tags=[
         "data-science",
